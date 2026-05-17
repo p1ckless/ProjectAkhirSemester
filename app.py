e7d5ce0e20ef5ee0664775df9337749958fe9727
p1ckless <putrasahaka434@gmail.com>
2026-05-17 15:49:14 +0700

    add chart to make chart for sales result for a week and fix the posisiton

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  y_hasilPenjualan = [10, 5, 25, 15, 35]
+  return render_template('index.html', data_penjualan=y_hasilPenjualan)
   
 if __name__=='__main__':
   app.run(debug=True)
