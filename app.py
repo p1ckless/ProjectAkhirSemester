821c885c9bb0da13b661e3892dc4b0e065fd5600
p1ckless <putrasahaka343@gmail.com>
2026-05-19 10:14:44 +0700

    add icon to card informatioan, and add variabel for card information

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+  totalProduk = 10
+  stokMenipis = 2
+  transaksiHariIni = 7
+  pendapatan = 1000000
   y_hasilPenjualan = [10, 5, 25, 15, 35]
-  return render_template('index.html', data_penjualan=y_hasilPenjualan)
+  return render_template('index.html', data_penjualan=y_hasilPenjualan, card_product=totalProduk, card_stock=stokMenipis, card_transaction=transaksiHariIni, card_income=pendapatan)
   
 if __name__=='__main__':
   app.run(debug=True)
